b05b95d5a6d764f9c0995ca594aeaf2e01525171
Dávid Urbančok <6781066+DavidUrbancok@users.noreply.github.com>
2023-07-03 20:56:11 +0200

    Added a basic contract project for logging

---
+++ src/Source Code/Core/Logging/RoboSum.Logging.Contracts/ILogger.cs
@@ -0,0 +1,27 @@
+﻿namespace RoboSum.Logging.Contracts
+{
+    /// <summary>
+    /// Defines a contract for logging messages.
+    /// </summary>
+    public interface ILogger
+    {
+        /// <summary>
+        /// Logs a message with the given <paramref name="severity"/>
+        /// using the <paramref name="messageTemplate"/> and the provided <paramref name="parameters"/>.
+        /// </summary>
+        /// <param name="severity">The logged message's severity.</param>
+        /// <param name="messageTemplate">The logged message template.</param>
+        /// <param name="parameters">The parameters to log.</param>
+        void Log(Severity severity, string messageTemplate, params object[] parameters);
+
+        /// <summary>
+        /// Logs a message with the given <paramref name="severity"/>
+        /// using the <paramref name="messageTemplate"/> and the provided <paramref name="parameters"/>.
+        /// </summary>
+        /// <param name="severity">The logged message's severity.</param>
+        /// <param name="exception">The exception to log.</param>
+        /// <param name="messageTemplate">The logged message template.</param>
+        /// <param name="parameters">The parameters to log.</param>
+        void Log(Severity severity, Exception exception, string messageTemplate, params object[] parameters);
+    }
+}
---
+++ src/Source Code/Core/Logging/RoboSum.Logging.Contracts/Severity.cs
@@ -0,0 +1,38 @@
+﻿namespace RoboSum.Logging.Contracts
+{
+    /// <summary>
+    /// Represents the severity of a logged message.
+    /// </summary>
+    public enum Severity
+    {
+        /// <summary>
+        /// The 'info' log severity.
+        /// </summary>
+        Info,
+
+        /// <summary>
+        /// The 'trace' log severity.
+        /// </summary>
+        Trace,
+
+        /// <summary>
+        /// The 'debug' log severity.
+        /// </summary>
+        Debug,
+
+        /// <summary>
+        /// The 'warning' log severity.
+        /// </summary>
+        Warning,
+
+        /// <summary>
+        /// The 'error' log severity.
+        /// </summary>
+        Error,
+
+        /// <summary>
+        /// The 'fatal' log severity.
+        /// </summary>
+        Fatal
+    }
+}
